6b42642525dbb433d2954056e707b124c5d3da6c
ITboo <annaacdc@tut.by>
2025-02-23 18:12:33 +0300

    chore: remove prisma context due to render issues

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -2,29 +2,24 @@ import express from "express";
 import cors from "cors";
 import * as trpcExpress from "@trpc/server/adapters/express";
 
-import { AppContext, createAppContext } from "./lib/ctx";
 import { trpcRouter } from "./router";
 import { applyTrpcToExpressApp } from "./lib/trpc";
 
 const PORT = 8080;
 
 void (async () => {
-  let ctx: AppContext | null = null;
-
   try {
-    ctx = createAppContext();
     const app = express();
 
     app.use(cors());
     app.get("/ping", (req, res) => {
       res.send("pong");
     });
-    applyTrpcToExpressApp(app, ctx, trpcRouter)
+    applyTrpcToExpressApp(app, trpcRouter)
     app.listen(PORT, () =>
       console.info(`Listening on http://localhost:${PORT}/`)
     );
   } catch (error) {
     console.error(error);
-    await ctx?.stop();
   }
 })();
