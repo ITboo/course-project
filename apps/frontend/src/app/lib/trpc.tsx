ab8b649ca725a503d4b62059d2971055cbfff2e3
ITboo <annaacdc@tut.by>
2025-02-25 21:53:09 +0300

    chore: fix link

--- apps/frontend/src/app/lib/trpc.tsx
+++ apps/frontend/src/app/lib/trpc.tsx
@@ -17,7 +17,7 @@ const queryClient = new QueryClient({
 const trpcClient = trpc.createClient({
   links: [
     httpBatchLink({
-      url: 'http://localhost:8080/forms',
+      url: 'https://course-project-x7ro.onrender.com/forms',
     }),
   ],
 });
